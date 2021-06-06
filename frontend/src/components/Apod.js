35145603f2e9d3c83f37007cb4937d6e3316b685
beooo79 <beoo79@gmail.com>
2021-06-07 01:30:09 +0200

    reload component Apod with button

--- frontend/src/components/Apod.js
+++ frontend/src/components/Apod.js
@@ -2,6 +2,7 @@ import { React, useEffect, useState } from "react";
 
 export const Apod = () => {
   const [data, setData] = useState([]);
+  const [resourceApod, setResourceApod] = useState(new Date());
 
   useEffect(() => {
     const fetchApod = async () => {
@@ -12,12 +13,13 @@ export const Apod = () => {
       }
     };
     fetchApod();
-  }, []);
+  }, [resourceApod]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3>Random Astronomy Picture of the Day</h3>
+        <button onClick={() => setResourceApod(new Date())}>Reload</button>
         <p className="App-text" float="left">
           {data.date}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{data.title}
         </p>
