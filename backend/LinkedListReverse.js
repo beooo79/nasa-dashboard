b4aa826e8f0655d05bff7343044bd16a08e921c8
beooo79 <beoo79@gmail.com>
2021-06-13 22:15:04 +0200

    reverse linked list

---
+++ backend/LinkedListReverse.js
@@ -0,0 +1,40 @@
+console.log("Reversing a Linked List")
+
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: {
+                    value: 5,
+                    next: {
+                        value: 6,
+                        next: null
+                    }
+                }
+            }
+        }
+    }
+};
+
+
+
+function reverse(list) {
+    let cur = list;
+    let prev = null;
+
+    while (cur != null) {
+        let next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+    }
+
+    return prev;
+}
+console.log(JSON.stringify(list, null, 2));
+
+console.log(JSON.stringify(reverse(list), null, 2));
